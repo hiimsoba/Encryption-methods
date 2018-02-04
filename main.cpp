4d3bc6d706be5330e0edc72eb58340171c14064a
hiimsoba <35265430+hiimsoba@users.noreply.github.com>
2018-02-04 10:39:34 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+bool subtract(char &c) {
+    if(islower(c)) {
+        c -= 'a' ;
+        return 1 ;
+    }
+    else {
+        c -= 'A' ;
+        return 0 ;
+    }
+}
+
+char add(char &c, bool lwr) {
+    if(lwr) {
+        return c + 'a' ;
+    }
+    else {
+        return c + 'A' ;
+    }
+}
+
+void createKey(string &key, string text) {
+    int index = 0 ;
+    while(key.size() != text.size()) {
+        if(isalpha(text[index])) {
+           key += text[index++] ;
+        }
+        else {
+            index++ ;
+        }
+    }
+}
+
+string compute(string input, int key) {
+    string res ;
+    for(int i = 0 ; i < input.size() ; i++) {
+        char c = input[i] ;
+        if(isalpha(c)) {
+            bool lwrc = subtract(c) ;
+            char r = c + key ;
+            r %= 26 ;
+            r += r < 0 ? 26 : 0 ;
+            res += add(r, lwrc) ;
+        }
+        else {
+            res += c ;
+        }
+    }
+    return res ;
+}
+
+int main() {
+    string text = "efgfoe uif fbtu xbmm pg uif dbtumf" ;
+    int key = 53 ;
+    bool option = 0 ;
+    /**
+        1 = encrypt
+        0 = decrypt
+    **/
+    key %= 26 ;
+    if(!option) {
+        key *= -1 ;
+    }
+    cout << compute(text, key) ;
+}
