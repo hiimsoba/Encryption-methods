f72999969846c4031c5506a327bf78013234ef64
hiimsoba <35265430+hiimsoba@users.noreply.github.com>
2018-12-27 19:49:22 +0200

    refactored and commented

--- caesar.cpp
+++ caesar.cpp
@@ -1,57 +1,54 @@
 #include <iostream>
 #include <string>
 
-using namespace std ;
-
+// normalize the 'c' character to be in the range 0-26 or something like that
+// and return true if it's lowercase, false if it's uppercase
 bool subtract(char &c) {
     if(islower(c)) {
-        c -= 'a' ;
-        return 1 ;
-    }
-    else {
-        c -= 'A' ;
-        return 0 ;
+        c -= 'a';
+        return true;
+    } else {
+        c -= 'A';
+        return false;
     }
 }
 
+// return the character at its initial state, keeping track of whether it was lowercase or uppercase
 char add(char &c, bool lwr) {
-    if(lwr) {
-        return c + 'a' ;
-    }
-    else {
-        return c + 'A' ;
-    }
+    return c + (lwr ? 'a' : 'A');
 }
 
-string compute(string input, int key) {
-    string res ;
-    for(int i = 0 ; i < input.size() ; i++) {
-        char c = input[i] ;
-        if(isalpha(c)) {
-            bool lwrc = subtract(c) ;
-            char r = c + key ;
-            r %= 26 ;
-            r += r < 0 ? 26 : 0 ;
-            res += add(r, lwrc) ;
-        }
-        else {
-            res += c ;
+// again, magic
+std::string compute(const std::string& input, int key) {
+    std::string res;
+    for(int i = 0; i < input.size(); i++) {
+        char c = input[i];
+        if (isalpha(c)) {
+            bool lwrc = subtract(c);
+            char r = c + key;
+            r %= 26;
+            r += r < 0 ? 26 : 0;
+            res += add(r, lwrc);
+        } else {
+            res += c;
         }
     }
-    return res ;
+    return res;
 }
 
 int main() {
-    string text = "efgfoe uif fbtu xbmm pg uif dbtumf" ;
-    int key = 53 ;
-    bool option = 0 ;
+    std::string text = "efgfoe uif fbtu xbmm pg uif dbtumf";
+    int key = 53;
+    bool option = 0;
     /**
         1 = encrypt
         0 = decrypt
     **/
-    key %= 26 ;
-    if(!option) {
-        key *= -1 ;
+    key %= 26;
+    if (!option) {
+        key *= -1;
     }
-    cout << compute(text, key) ;
+
+    std::cout << compute(text, key);
+    return 0;
 }
