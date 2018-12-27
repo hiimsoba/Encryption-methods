5a753afddf7c35cf6340812eaed174b22a9bdab4
hiimsoba <35265430+hiimsoba@users.noreply.github.com>
2018-12-27 19:32:43 +0200

    updated code and added comments

--- autokey.cpp
+++ autokey.cpp
@@ -1,75 +1,85 @@
 #include <iostream>
 #include <string>
 
-using namespace std ;
-
+// normalize the 'c' character to be in the range 0-26 or something like that
+// and return true if it's lowercase, false if it's uppercase
 bool subtract(char &c) {
-    if(islower(c)) {
-        c -= 'a' ;
-        return 1 ;
-    }
-    else {
-        c -= 'A' ;
-        return 0 ;
+    if (islower(c)) {
+        c -= 'a';
+        return true;
+    } else {
+        c -= 'A';
+        return false;
     }
 }
 
+// return the character at its initial state, keeping track of whether it was lowercase or uppercase
 char add(char &c, bool lwr) {
-    if(lwr) {
-        return c + 'a' ;
-    }
-    else {
-        return c + 'A' ;
-    }
+    return c + (lwr ? 'a' : 'A');
 }
 
-void createKey(string &key, string text) {
-    int index = 0 ;
-    while(key.size() != text.size()) {
-        if(isalpha(text[index])) {
-           key += text[index++] ;
-        }
-        else {
-            index++ ;
+std::string createKey(const std::string& key, const std::string& text) {
+    // create the key from the "seed" key
+    std::string new_key;
+    for (size_t i = 0; i < key.size(); i++) {
+        // only add the letter characters from the new key
+        if (isalpha(key[i])) {
+            new_key += key[i];
         }
     }
+    // and then keep adding characters from the plain text to the key until the key and the plain text have the same size
+    int index = 0;
+    while (new_key.size() != text.size()) {
+        // only add if it's a letter, as well
+        if (isalpha(text[index])) {
+            new_key += text[index++];
+        } else {
+            index++;
+        }
+    }
+    // return the key
+    return new_key;
 }
 
-string compute(string input, string key, bool option) {
-    int kIndex = 0 ;
-    string res ;
-    for(int i = 0 ; i < input.size() ; i++) {
-        char c = input[i] ;
-        char k = key[kIndex] ;
-        if(isalpha(c)) {
-            bool lwrc = subtract(c) ;
-            bool lwrk = subtract(k) ;
-            char r = option ? k + c : c - k ;
-            r += r < 0 ? 26 : 0 ;
-            r -= r >= 26 ? 26 : 0 ;
-            res += add(r, lwrc) ;
-            if(!option && key.size() != input.size()) {
-                key += add(r, lwrc) ;
+// magic...
+std::string compute(std::string input, std::string key, bool option) {
+    int kIndex = 0;
+    std::string res;
+    for (size_t i = 0; i < input.size(); i++) {
+        char c = input[i];
+        char k = key[kIndex];
+        if (isalpha(c)) {
+            bool lwrc = subtract(c);
+            subtract(k);
+            char r = option ? k + c : c - k;
+            r += r < 0 ? 26 : 0;
+            r -= r >= 26 ? 26 : 0;
+            res += add(r, lwrc);
+            if (!option && key.size() != input.size()) {
+                key += add(r, lwrc);
             }
-            kIndex++ ;
-        }
-        else {
-            res += c ;
+            kIndex++;
+        } else {
+            res += c;
         }
     }
-    return res ;
+    return res;
 }
 
 int main() {
-    string text = "ATTACK THE EAST WALL AT DAWN" ;
-    string key = "QUEEN" ;
-    bool option = 1 ;
+    std::string text = "ATTACK THE EAST WALL AT DAWN";
+    std::string key = "QUEEN";
+
     /**
         1 = encrypt
         0 = decrypt
     **/
-    if(option) {
-        createKey(key, text) ;
+    bool option = 1;
+
+    if (option) {
+        key = createKey(key, text);
     }
-    cout << compute(text, key, option) ;
+
+    std::cout << compute(text, key, option);
+    return 0;
 }
